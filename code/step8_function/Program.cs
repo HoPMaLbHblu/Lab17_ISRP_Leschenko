af9affdfdb4226bc5b9f36f2187316a97a32c59e
Dan <vfvf5389127@gmail.com>
2026-03-29 13:55:37 +0300

    Step 8: functions and modular programming

---
+++ code/step8_function/Program.cs
@@ -0,0 +1,60 @@
+﻿int num1, num2;
+string? operation;
+
+Console.Write("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите операцию (+, -, *, /): ");
+operation = Console.ReadLine();
+
+if (operation == "+")
+{
+    Console.WriteLine($"Результат: {Sum(num1, num2)}");
+}
+else if (operation == "-")
+{
+    Console.WriteLine($"Результат: {Subtract(num1, num2)}");
+}
+else if (operation == "*")
+{
+    Console.WriteLine($"Результат: {Multiply(num1, num2)}");
+}
+else if (operation == "/")
+{
+    Console.WriteLine($"Результат: {Divide(num1, num2)}");
+}
+else
+{
+    Console.WriteLine("Ошибка: неверная операция");
+}
+
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+int Subtract(int a, int b)
+{
+    return a - b;
+}
+
+int Multiply(int a, int b)
+{
+    return a * b;
+}
+
+double Divide(int a, int b)
+{
+    if (b != 0)
+    {
+        return (double)a / b;
+    }
+    else
+    {
+        Console.WriteLine("Ошибка: деление на ноль");
+        return 0;
+    }
+}
---
+++ code/step8_prime/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (IsPrime(number))
+{
+    Console.WriteLine($"{number} - простое число");
+}
+else
+{
+    Console.WriteLine($"{number} - не простое число");
+}
+
+bool IsPrime(int n)
+{
+    if (n < 2) return false;
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
