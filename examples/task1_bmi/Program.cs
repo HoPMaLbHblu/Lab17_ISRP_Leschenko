de49f1524b5694804ccf028de8e3071ba8cd87e9
Dan <vfvf5389127@gmail.com>
2026-03-29 14:15:21 +0300

    Finished task1 BMI calculator

---
+++ examples/task1_bmi/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Console.Write("Введите ваш вес (кг): ");
+double weight = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите ваш рост (м): ");
+double height = Convert.ToDouble(Console.ReadLine());
+
+double bmi = weight / (height * height);
+string category = "";
+string recommendation = "";
+
+if (bmi < 18.5)
+{
+    category = "Недостаточный вес";
+    recommendation = "Рекомендуется увеличить калорийность питания и обратиться к врачу.";
+}
+else if (bmi < 25)
+{
+    category = "Нормальный вес";
+    recommendation = "Отлично! Поддерживайте здоровый образ жизни.";
+}
+else if (bmi < 30)
+{
+    category = "Избыточный вес";
+    recommendation = "Рекомендуется увеличить физическую активность и пересмотреть питание.";
+}
+else
+{
+    category = "Ожирение";
+    recommendation = "Рекомендуется обратиться к врачу для составления плана снижения веса.";
+}
+
+Console.WriteLine("\nРЕЗУЛЬТАТ:");
+Console.WriteLine($"Ваш ИМТ: {bmi:F2}");
+Console.WriteLine($"Категория: {category}");
+Console.WriteLine($"Рекомендация: {recommendation}");
